977c71257ac4434d250ffb4c5ae82491825890e3
mireylock <117910606+mireylock@users.noreply.github.com>
2024-03-05 13:23:49 +0100

    .

--- one_to_one_jpa/src/main/java/org/iesvdm/one_to_one_jpa/repository/SocioRepository.java
+++
@@ -1,7 +0,0 @@
-package org.iesvdm.one_to_one_jpa.repository;
-
-import org.iesvdm.one_to_one_jpa.domain.Socio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SocioRepository extends JpaRepository<Socio, Long> {
-}
